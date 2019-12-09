c86643db0f32a7ff02124d5fda89b7e2e3fcfaf1
BeiChen Zhang <45487945+Bosh0113@users.noreply.github.com>
2019-12-09 17:19:53 +0800

    代码优化

--- Subcatchments_Width.py
+++ Subcatchments_Width.py
@@ -22,16 +22,11 @@ try:
     # Use MinimumBoundingGeometry function to get a convex hull area
     arcpy.MinimumBoundingGeometry_management(outShpTempPath, outputBoundary, "RECTANGLE_BY_AREA", "NONE")
     # Execute AddField
-    arcpy.AddField_management(outputBoundary, "Area", "DOUBLE", "", "", "", "area", "NULLABLE", "NON_REQUIRED", "")
-    arcpy.AddField_management(outputBoundary, "Perimeter", "DOUBLE", "", "", "", "perimeter", "NULLABLE", "NON_REQUIRED"
-                              , "")
     arcpy.AddField_management(outputBoundary, "MaxEdge", "DOUBLE", "", "", "", "maxEdge", "NULLABLE", "NON_REQUIRED"
                               , "")
-    # Calculate Area and Perimeter
-    arcpy.CalculateField_management(outputBoundary, "Area",  "!shape.geodesicArea@METERS!", "PYTHON")
-    arcpy.CalculateField_management(outputBoundary, "Perimeter",  "!shape.geodesicLength@METERS!", "PYTHON")
-    # Calculate max edge
-    expression = "(!Perimeter! + math.sqrt(math.pow(!Perimeter!, 2) - 16 * !Area!))/4"
+    # Calculate Max Edge
+    expression = "(!shape.geodesicLength@METERS! + math.sqrt(math.pow(!shape.geodesicLength@METERS!, 2) - 16 *" \
+                 " !shape.geodesicArea@METERS!))/4"
     arcpy.CalculateField_management(outputBoundary, "MaxEdge",  expression, "PYTHON")
     # Execute AddField
     arcpy.AddField_management(outShpTempPath, "Width", "DOUBLE", "", "", "", "slope", "NULLABLE", "NON_REQUIRED", "")
@@ -40,7 +35,7 @@ try:
     arcpy.MakeFeatureLayer_management(outShpTempPath, layerName)
     # Join the feature layer to a table
     arcpy.AddJoin_management(layerName, indexField, outputBoundary, indexField)
-    # Calculate Area
+    # Calculate Width
     arcpy.CalculateField_management(layerName, "Width",  "!shape.geodesicArea@METERS!/!boundary.MaxEdge!", "PYTHON")
     # Remove the join
     arcpy.RemoveJoin_management(layerName, "boundary")
